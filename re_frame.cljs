3ceea7df89e27279824bebd1ef62d2e718a4baf6
bonuoq <154889941+bonuoq@users.noreply.github.com>
2023-12-30 01:47:40 +0100

    Update re_frame.cljs

--- re_frame.cljs
+++ re_frame.cljs
@@ -8,20 +8,6 @@
             [pou.core :as p]
             [applied-science.js-interop :as j]))
 
-(defn pou [& ks] (eval (conj ks deref `app-db `some->)))
-    
-(rf/reg-event-db
- :initialize
- (fn [_ _]  
-   {:params (or (js/klipse.utils.url-parameters) {})
-    :editors {}}))
-
-(rf/reg-event-db
- :reg-editor-comp
- (fn [db [_ editor]]
-   (let [c @klp/snippet-counter]
-     (update-in db [:editors] merge {c editor}))))
-
 (rf/reg-sub
  :editors
  (fn [db _]
@@ -33,25 +19,46 @@
    (get-in db [:editors idx])))
 
 (defn editor-comp [{:keys [mode attrs snippet klipsettings] :or {klipsettings {}}}]
-  (r/create-class
-    {:component-did-mount
-     (fn [this]
-       (klp/klipsify (rdom/dom-node this) klipsettings mode))               
-     :reagent-render
-     (fn []
-       [:div.pou-editor attrs (str snippet)])}))
+  (let [s (r/atom {:visible? true})]
+    (r/create-class
+      {:component-did-mount
+       (fn [this]
+         (klp/klipsify (. (rdom/dom-node this) querySelector ".pou-editor") klipsettings mode))               
+       :reagent-render
+       (fn []
+         (let [visible? (:visible? @s)]
+           [:div.pou-toolbar {}
+            [:button.toggle-min
+             {:on-click #(swap! s update :visible? not)}
+             (if visible? "-" "+")]]
+           [:div.pou-wrapper 
+            {:style {:display (if visible? "block" "none")}}
+            [:div.pou-editor attrs (str snippet)]])}))))
 
 (defn append-editor [& {:keys [mode] :or {mode "eval-clojure"} :as editor-map}]
-  (let [editor (merge editor-map {:mode mode})]
+  (let [editor (assoc editor-map :mode mode)
+        idx @klp/snippet-counter]
     (p/reg-editor editor)
-    (rf/dispatch [:reg-editor-comp [editor-comp editor]])))
+    (rf/dispatch [:reg-editor-comp {idx [editor-comp editor]}])))
 
 (defn pou-re-frame []
   (let [editors @(rf/subscribe [:editors])]
     [:div#pou-editors
      (for [e editors]
-       @(rf/subscribe [:editor-comp (-> e key)]))]))
+       (let [idx (key e)]
+         ^{:key idx} @(rf/subscribe [:editor-comp idx])))]))
+
+(rf/reg-event-db
+ :reg-editor-comp
+ (fn [db [_ editor]]
+   (update-in db [:editors] conj editor)))
+
+(rf/reg-event-db
+ :initialize
+ (fn [_ _]  
+   {:params (or (js/klipse.utils.url-parameters) {})
+    :editors {}}))
 
 (rdom/render [pou-re-frame] (gdom/getElement "app"))
-
+(rf/dispatch [:initialize])
   
