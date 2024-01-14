c6b73fc68b92c355c7a9d2e10007c16c4b48f28a
bonuoq <154889941+bonuoq@users.noreply.github.com>
2024-01-14 14:34:02 +0100

    Update core.cljs

--- core.cljs
+++ core.cljs
@@ -8,7 +8,9 @@
 
 (def url-params (or (klu/url-parameters) {}))
 
-(defn append-editor [{:keys [mode attrs snippet klipsettings] :or {mode "eval-clojure" klipsettings {}} :as editor-map}]
+(defn append-editor [{:keys [mode attrs snippet klipsettings] 
+                      :or {mode "eval-clojure" klipsettings {}} 
+                      :as editor-map}]
   (let [editor (update-in editor-map [:attrs :data-external-libs] #(or % "https://bonuoq.github.io"))
         div (gdom/createDom "div" (clj->js (:attrs editor)) (gdom/createTextNode (str snippet)))
         idx @klp/snippet-counter
@@ -17,7 +19,8 @@
     (gdom/insertSiblingAfter label js/klipse-container.nextSibling)
     (klp/klipsify div klipsettings mode)))
 
-(defn addp [snippet & kvs] (append-editor (assoc (apply hash-map kvs) :snippet snippet)))
+(defn addp [snippet & {:keys [mode attrs snippet klipsettings] :as editor-settings}] 
+  (append-editor (assoc (apply hash-map editor-settings) :snippet snippet)))
                                                             
 (defn call-in-editor [k method & args]
   (j/apply (@kleds/editors k) method (clj->js args)))
@@ -49,31 +52,22 @@
 (defn trigger-eval [k] 
   (set-code k (call-in-editor k :getValue)))
 
-(defn fetch-url-text [url callback]
+(defn fetch-url [url callback]
   (-> (str url) js/fetch
-    (.then (fn [r] (.. r text (then callback))))))
-
-(defn eval-url [url & {:keys [editor append-code clear-code] :or {editor 0 append-code ""}}]
-  (fetch-url-text url #(set-code editor (str % "\n" append-code
-                                             (when clear-code (str "\n(pou.user/set-code " editor " \"" clear-code "\")"))))))
+    (.then #(callback %))))
     
 (defn fetch-gist [id file callback]
-  (-> (str "https://api.github.com/gists/" id) js/fetch
-    (.then (fn [r]
-       (.then (.json r)
-              (fn [json]
-                (callback (-> (js->clj json :keywordize-keys true) :files ((keyword file)) :content))))))))
+  (-> (str "https://api.github.com/gists/" id)
+    (fetch-url 
+     #(-> (.json %)
+        (.then
+          (fn [json]
+            (callback (-> (js->clj json :keywordize-keys true) :files ((keyword file)) :content))))))))
 
-(defn append-gist [{:keys [id file append-code clear-code] :or {append-code ""}}]
-    
-(defn eval-gist [{:keys [id file editor append-code clear-code] :or {editor 0 append-code ""}}]
-  (fetch-gist id file #(set-code editor (str % "\n" append-code
-                                             (when clear-code (str "\n(pou.user/set-code " editor " \"" clear-code "\")"))))))
-
-(defn eval-gists [f & r]
-  (eval-gist (assoc f :append-code (if (not (empty? (rest r)))
-                                     `(apply eval-gists ~r)
-                                     `(apply eval-gist ~(first r))))))
+(defn append-gist [{:keys [id file mode attrs klipsettings append-code] 
+                    :or {mode "eval-clojure" append-code ""} 
+                    :as editor}]
+  (fetch-gist id file #(addp (str % append-code) editor)))
 
 (defn process-url-params [& param-procs]
   (doseq [pp (partition 2 param-procs)]
