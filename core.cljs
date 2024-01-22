1bdfad58896b8c9b36e0b69b89dbad3d900568e7
bonuoq <154889941+bonuoq@users.noreply.github.com>
2024-01-22 16:59:13 +0100

    Update core.cljs

--- core.cljs
+++ core.cljs
@@ -230,7 +230,8 @@
    (let [{:keys [status body]}
          (<! (http/post (str "https://cors-anywhere.herokuapp.com/" ; for development purposes
                              "https://github.com/login/oauth/access_token")
-                        {:headers {"Accept" "application/json"}
+                        {:with-credentials? false
+                         :headers {"Accept" "application/json"}
                          :json-params {:client_id "ecde871676236cae5c25"
                                        :client_secret "38d46c164985bf82f9b617f7d0cd95633026ac48"
                                        :code code
