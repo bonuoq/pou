5593b280d345164bd6d1a3fc4890c107a7b44b0f
bonuoq <154889941+bonuoq@users.noreply.github.com>
2024-01-23 12:38:28 +0100

    Update core.cljs

--- core.cljs
+++ core.cljs
@@ -235,23 +235,6 @@
   (loading!)
   (load-module (str "ui/" ui) :on-ready #(loaded!)))
 
-(defn github-auth [code]
-  (.replaceState js/history {} "" "/pou")
-  (go
-   (let [{:keys [status body]}
-         (<! (http/post (str "https://cors-anywhere.herokuapp.com/" ; for development purposes
-                             "https://github.com/login/oauth/access_token")
-                        {:with-credentials? false
-                         :headers {"Accept" "application/json"}
-                         :json-params {:client_id "ecde871676236cae5c25"
-                                       :client_secret "38d46c164985bf82f9b617f7d0cd95633026ac48"
-                                       :code code
-                                       :redirect_uri "https://bonuoq.github.io/pou/"}}))]
-     (swap! pou assoc :github (js->clj body :keywordize-keys true))
-     (let [{:keys [login avatar_url]}
-           (<! (request "user" :login :avatar_url))]
-       (update-div (str "<span class='gh-login'><img class='gh-avatar' src='" avatar_url "'>" login "</span>"))))))
-
 (defn init! []
   (process-url-params :ui #(load-ui %)
                       :editor-base #(append [(parse64 %)])
@@ -259,5 +242,5 @@
                       :p #(aed (decode64 %))
                       :module #(load-module %)
                       :modules #(apply load-modules (parse64 %))
-                      :code #(github-auth %))
+                      :code #(load-module 'github))
   (when-not (:ui url-params) (loaded!)))
