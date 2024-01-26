e61c3697a4c2adf325d4704b413c119ddaedf1cb
bonuoq <154889941+bonuoq@users.noreply.github.com>
2024-01-26 16:09:03 +0100

    Update core.cljs

--- core.cljs
+++ core.cljs
@@ -256,9 +256,9 @@
    :mode "eval-clojure"
    :attrs {:class "pou-wrapper"}})
 
-(defn append [editors & {:keys [provide override klipsify? on-mounted on-ready]
-                         :or {klipsify? (let [ui (some :ui '(provide default-keys))]
-                                          (some-> @pou :uis ui :klipsify?))}}]
+(defn append [editors & {:keys [provide override klipsify? on-mounted on-ready]}]
+                         ;:or {klipsify? (let [ui (some :ui '(provide default-keys))]
+                                          ;(some-> @pou :uis ui :klipsify?))}}]
   #_(dotimes [n (count editors)]
     (let [{:keys [id from-gist] :as specific} (get editors n)
           a (js/console.log (clj->js specific))
