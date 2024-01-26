91ec68a46d76f55c0125483cefcdaa7fa8a3c973
bonuoq <154889941+bonuoq@users.noreply.github.com>
2024-01-27 00:02:02 +0100

    Update core.cljs

--- core.cljs
+++ core.cljs
@@ -358,7 +358,7 @@
 
 (defn request [path & {:keys [callback selected-keys pre-path]}] ; todo return channel
   (go
-   (let [{:keys [status body]} (<! (http/get (str pre-path path) {:with-credentials? false}))]
+   (let [{:keys [status body]} (<! (http/get (str pre-path path) {:with-credentials? false}))
          res (if (= status 200)
                (if (not-empty selected-keys)
                  (if (vector? body)
