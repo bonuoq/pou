ffc5d77bcab3c9bbafb8767f148c850fc8584c9f
bonuoq <154889941+bonuoq@users.noreply.github.com>
2024-01-29 16:50:02 +0100

    Update github.cljs

--- modules/github.cljs
+++ modules/github.cljs
@@ -68,8 +68,7 @@
                          :headers {"Accept" "application/json"}
                          :json-params {:client_id "ecde871676236cae5c25"
                                        :client_secret "38d46c164985bf82f9b617f7d0cd95633026ac48"
-                                       :code code
-                                       :redirect_uri "https://bonuoq.github.io/pou/github-callback?action=close"}}))]
+                                       :code code}}))]
      (js/console.log (str "GitHub Auth response (" status "): " body))
      (if (= status 200)
        (logged! body)
